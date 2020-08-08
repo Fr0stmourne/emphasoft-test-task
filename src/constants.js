bdc797b75a386454cd817ee9eccbb61dfdcd9250
Frostmourne <cozroman@yandex.ru>
2020-08-08 15:05:01 +0300

    fix: fix of an env variable

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,8 @@ const proxy = `https://cors-anywhere.herokuapp.com/`;
 
 const clientID = 7560327;
 const clientSecret = process.env.REACT_APP_CLIENT_KEY;
-const redirectCallbackUrl = process.env.REACT_APP_URL || 'http://localhost:3000/auth/vkontakte/callback';
+const localhostCallbackUrl = 'http://localhost:3000';
+const redirectCallbackUrl = `${process.env.REACT_APP_URL || localhostCallbackUrl}/auth/vkontakte/callback`;
 const tokenKey = 'vk-token';
 
 export {
